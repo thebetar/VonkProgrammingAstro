c866e7d7cb797ef9fbe7746e543d4bee714bd75f
Lars Vonk <36197012+thebetar@users.noreply.github.com>
2024-04-19 19:07:22 +0200

    chore: Update info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -6,7 +6,7 @@ export const info = {
 	about: `
     As a software enthusiast, I value quality code above all. My commitment extends to robust linting, rigorous unit testing, adherence to code standards, and efficient pipeline management. Every line of code I write embodies my dedication to excellence and precision in software development.
   `,
-	keywords: 'Vue, React, Javascript, Express.js, MongoDB',
+	keywords: 'web development, mobile development, freelance developer, Bussum, Naarden, Hilversum, Netherlands, front-end development, Vue, React',
 
 	skills: [
 		{
